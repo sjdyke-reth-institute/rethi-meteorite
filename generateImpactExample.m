09060e4f86ba933c54a865706bdf1e4ef0d6b621
starmaid <nicholas.masso.14@gmail.com>
2020-11-16 12:15:23 -0500

    output location, better docs

--- generateImpactExample.m
+++ generateImpactExample.m
@@ -1,4 +1,14 @@
-
 % this is how to format a GenerateImpactData line
 % this generates the required .mat files, with appropriate names
-generateImpactData(200000, 200000, 0, 0, 1/(365*24), "today")
+% also generates a .csv file with the same data
+
+xlength = 200000; % meters
+zlength = 200000; % meters
+latitude = 0; % degrees
+longitude = 0; % degrees
+duration = 1/(365*24); % years
+startdate = "1-Dec-2020"; % datestring
+
+generateImpactData(zlength, xlength, latitude, longitude, duration, startdate);
+
+clear
